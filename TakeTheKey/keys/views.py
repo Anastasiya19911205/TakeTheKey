259cb9ac6dc07f2bb7870e9ff31ed892f726608d
Anastasiya <myhopadik@mail.ru>
2023-07-15 21:32:49 +0300

    login

--- TakeTheKey/keys/views.py
+++ TakeTheKey/keys/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView
+from .utils import *
+
+
 def enter(request):
     return render(request, "enter.html")
 
@@ -12,19 +17,43 @@ def activate (request):
 def feedback (request):
     return render(request, "feedback.html")
 
+def login (request):
+    return render(request, "login.html")
+
 def package_activation (request):
     return render(request, "package_activation.html")
 
 
-
-# class LoginUser(LoginRequiredMixin, LoginView):
-#     form_class = AuthenticationForm
+# class Register(DataMixin, CreateView):
+#     form_class = RegisterUserForm
 #     template_name = 'keys/enter.html'
+#     success_url = reverse_lazy('login')
 #
 #     def get_context_data(self, *, object_list=None, **kwargs):
 #         context = super().get_context_data(**kwargs)
-#         c_def = self.get_user_context(title="Авторизация")
+#         c_def = self.get_user_context(title="Регистрация")
 #         return dict(list(context.items())+list(c_def.items()))
-#
-#     def get_user_context(self, title):
-#         pass
+
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = UserCreationForm
+    template_name = 'keys/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items())+list(c_def.items()))
+
+class LoginUser(DataMixin, LoginView):
+    form_class = AuthenticationForm
+    template_name = 'keys/login.html'
+
+    def get_context_data(self, *,object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items())+list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('package_activation')
